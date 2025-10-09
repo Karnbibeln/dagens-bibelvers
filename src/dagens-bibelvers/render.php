e2737a9beac041b775870eb96a4b870d5dc28791
Samuel Grenehed <sag05@duck.com>
2025-10-09 11:04:12 +0200

    added abspath check to render.php

--- src/dagens-bibelvers/render.php
+++ src/dagens-bibelvers/render.php
@@ -2,8 +2,10 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
-<p <?php echo wp_kses_data(get_block_wrapper_attributes()); ?>>
+<p 
+<?php echo wp_kses_data(get_block_wrapper_attributes()); ?>>
     <?php
     $url = 'https://www.karnbibeln.se/app/v1/dailyverse_json/' . gmdate('Y') . '.json';
     $json = wp_remote_get($url)["body"];
